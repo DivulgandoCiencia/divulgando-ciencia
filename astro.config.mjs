cc9d7dc68adb08995e5c3f18f60763856bbe2b81
Neplod <dorian.ruvel@gmail.com>
2025-10-25 21:23:49 +0200

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel';
-import node from '@astrojs/node'
 import mdx from '@astrojs/mdx';
-
-let adapter = vercel();
-
-if (process.argv[3] === "--node" || process.argv[4] === "--node") {
-  adapter = node({ mode: "standalone" });
-}
 
 export default defineConfig({
   output: 'server',
@@ -19,5 +12,5 @@ export default defineConfig({
     syntaxHighlight: false,
   },
 
-  adapter: adapter,
+  adapter: vercel(),
 });
