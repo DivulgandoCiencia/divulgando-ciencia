13ceb23c81dc048ac497a9c3a2a519f63a79f696
Neplod <dorian.ruvel@gmail.com>
2026-05-31 19:40:00 +0200

    Minor Version 5.0.4
    added:
     - Page Load Event Dispatch Repetition Preventer

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // === CONFIGURACIÓN ===
-const CACHE_NAME = 'dC-cache-v5.0.3'; 
+const CACHE_NAME = 'dC-cache-v5.0.4'; 
 const ASSETS_TO_CACHE = [
     '/images/logo.webp',
     '/images/landing-page/banner.webp',
